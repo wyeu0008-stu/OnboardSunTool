b1c670581c593343fbc37d6cf1c1149d0e5f907a
wyeu0008 <wyeu0008@student.monash.edu>
2026-03-13 01:28:02 +1100

    Add Guide Page and entry

---
+++ src/components/PersonalSkinRisk.js
@@ -0,0 +1,10 @@
+export default {
+  name: 'PersonalSkinRisk',
+  template: `
+    <section class="card">
+      <h3>Personal Skin Risk</h3>
+      <p>Skin Type: Type III</p>
+      <p>Sunburn Risk: 65%</p>
+    </section>
+  `
+}
