0433e41d210c38aec489a7e570ab84122ae61909
academic8878 <academic8878@gmail.com>
2026-03-16 19:43:05 +1100

    Epic 2: awareness dashboard and personalised skin protection

--- src/components/UVClothingGuide.js
+++ src/components/UVClothingGuide.js
@@ -1,11 +1,54 @@
 export default {
   name: 'UVClothingGuide',
+  props: {
+    selectedSkinType: {
+      type: String,
+      default: 'Type III'
+    }
+  },
+  computed: {
+    clothingAdvice() {
+      const map = {
+        'Type I': [
+          'Wide-brim hat',
+          'UPF 50+ long-sleeve shirt',
+          'UV400 sunglasses',
+          'Stay in shade where possible'
+        ],
+        'Type II': [
+          'Wide-brim hat',
+          'UPF 50+ shirt',
+          'UV400 sunglasses',
+          'Cover shoulders during high UV periods'
+        ],
+        'Type III': [
+          'Hat or cap with shade coverage',
+          'Light long-sleeve shirt',
+          'UV400 sunglasses'
+        ],
+        'Type IV': [
+          'Hat for long outdoor activities',
+          'Comfortable protective shirt',
+          'UV400 sunglasses'
+        ],
+        'Type V': [
+          'Sunglasses',
+          'Hat for extended sun exposure',
+          'Comfortable protective clothing'
+        ]
+      }
+      return map[this.selectedSkinType] || map['Type III']
+    }
+  },
   template: `
     <section class="card">
       <h3>UV-Based Clothing Guide</h3>
-      <p>Wide-brim hat</p>
-      <p>UPF 50+ shirt</p>
-      <p>UV400 sunglasses</p>
+      <p>
+        Your selected skin type may change how carefully you need to protect yourself during high UV periods.
+      </p>
+      <ul>
+        <li v-for="item in clothingAdvice" :key="item">{{ item }}</li>
+      </ul>
     </section>
   `
 }
