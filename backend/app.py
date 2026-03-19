5eff467a744d0e6016571e002b2674ec68f5551b
wyeu0008-stu <wyeu0008@student.monash.edu>
2026-03-19 22:10:20 +1100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -43,4 +43,4 @@ def postcodes_api():
         return jsonify({"error": True, "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, , use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
